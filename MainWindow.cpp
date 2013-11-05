b9560bb820f1aa77b25eedb6fb992ca6774d53cc
Klaudiusz <klaudiusz.stawski@gmail.com>
2013-11-05 17:36:37 +0100

    Fixed bug with binarize not loaded image

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 	normalImage = NULL;
 	filtredImage = NULL;
 	connect(ui.loadImgButton,SIGNAL(clicked()),this,SLOT(loadNormalImage()));
-	connect(ui.executeButton,SIGNAL(clicked()),this,SLOT(binarizeImage())); // FIXME
+	connect(ui.executeButton,SIGNAL(clicked()),this,SLOT(binarizeImage()));
 	connect(ui.intensySlider,SIGNAL(valueChanged(int)),ui.intensyValue,SLOT(setNum(int)));
 }
 
@@ -26,24 +26,28 @@ QString MainWindow::getFilePath(QString dirPath ,QString fileFilter)
 void MainWindow::loadNormalImage()
 {
 	ui.filePathLine->setText(getFilePath("C:"));
-	if(filtredImage) // if oldImage had existed before
-		delete filtredImage;
-	filtredImage =  new QImage(ui.filePathLine->text());
-	setLabelImg(filtredImage->copy(),ui.oldImgLabel);
-	setEnableLayoutIthems(ui.executeHLayout);
+	if(normalImage) // if oldImage had existed before
+		delete normalImage;
+	normalImage =  new QImage(ui.filePathLine->text());
+	ui.newImgLabel->clear();
+	setLabelImg(normalImage->copy(),ui.oldImgLabel);
+	setEnableLayoutIthems(ui.executeHLayout,true);
 }
 
 void MainWindow::binarizeImage()
 {
-	if(normalImage) // if newImage had existed before
-		delete normalImage;
-	normalImage = new QImage(filtredImage->copy());
-	if(ui.comboBox->currentText() == "ASM")
-		Filter::asmBinaryzation(normalImage,ui.intensySlider->value());
-	else
-		Filter::cppBinaryzation(normalImage, ui.intensySlider->value());
+	if(normalImage) // if image didn't load function can't binarize
+	{
+		if(filtredImage) // if newImage had existed before
+			delete filtredImage;
+		filtredImage = new QImage(normalImage->copy());
+		if(ui.comboBox->currentText() == "ASM")
+			Filter::asmBinaryzation(filtredImage,ui.intensySlider->value());
+		else
+			Filter::cppBinaryzation(filtredImage, ui.intensySlider->value());
 
-	setLabelImg(normalImage->copy(), ui.newImgLabel);
+		setLabelImg(filtredImage->copy(), ui.newImgLabel);
+	}
 }
 
 void MainWindow::setLabelImg(QImage source, QLabel * destination, bool scaleToWidth)
